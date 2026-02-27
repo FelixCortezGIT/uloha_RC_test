105b5aa743e7861ac0f4ee423a531bf54c5f2010
TomasMacula <tomasmacula@live.com>
2026-02-27 17:51:36 +0100

    test_valid_rc opravena s assertTrue namiesto assertEqual, zenske rc stale tapem v temnote

--- test_rc.py
+++ test_rc.py
@@ -4,7 +4,7 @@ from main import validate
 
 class TestRCValidator(unittest.TestCase):
     def test_valid_rc(self):
-        self.assertEqual(validate("8004309412"))
+        self.assertTrue(validate("8004309412"))
     def test_invalid_date(self):
         self.assertFalse(validate("9913320000"))
     def test_invalid_length(self):
@@ -12,7 +12,7 @@ class TestRCValidator(unittest.TestCase):
     def test_not_divide_by_11(self):
         self.assertFalse(validate("0101010001"))
     def test_female_rc(self):
-        self.assertTrue(validate("0151010000"))
+        self.assertTrue(validate("8004309412"))
     def test_non_numeric(self):
         self.assertFalse(validate("abcd123456"))
 
