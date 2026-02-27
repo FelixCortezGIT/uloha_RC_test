c629d8f5aeda694a92f0d033768d544105cc7fae
TomasMacula <tomasmacula@live.com>
2026-02-27 17:12:46 +0100

    rc uvediem ako str a bool aby bolo len pravdive alebo nepravdive pridana funkcia overujuca ci rc je cislo, ci ma 10 cisel, delitelnost 11-timi, zenske rc, uvedenie ktore cislice predstavuju rok, mesiac a den, kontrola platnosti datumu konzola na overenie kodu Code inspection did not find anything to report. 15 files processed in 'Project 'uloha_RC_test''.

---
+++ main.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+def validate(rc: str) -> bool:
+    if not rc.isdigit():
+        return False
+    if len(rc) != 10:
+        return False
+    if int(rc) % 11 != 0:
+        return False
+    year = int(rc[:2])
+    month = int(rc[2:4])
+    day = int(rc[4:6])
+    if month > 50:
+        month -= 50
+    try:
+        datetime(1900 + year, month, day)
+    except ValueError:
+        return False
+    return True
+
+
+print(validate("0101010000"))
+print(validate("9913320000"))
+print(validate("0151010000"))
+print(validate("abcd123456"))
+print(validate("8004309412"))
